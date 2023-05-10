0b985a02259bb63b51e75ab0d9c56b6a67bb5d58
Shashank Kumar <shashank9592sinha@gmail.com>
2023-05-10 20:12:12 +0530

    Created Folder Structure

---
+++ src/main/java/com/movies/MoviesRatingsWebService/MoviesRatingsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.movies.MoviesRatingsWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRatingsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRatingsWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movies/MoviesRatingsWebService/MoviesRatingsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.MoviesRatingsWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRatingsWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
